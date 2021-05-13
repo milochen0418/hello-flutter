9edbf076cc6ddbd71ed088395ceb876abd75ae67
milochen0418 <milo_chen@mail2000.com.tw>
2021-05-14 00:28:41 +0800

    List View to show items

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -73,40 +73,18 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            ListTile(
-                leading: const Icon(Icons.phone_android),
-                title: const Text("Android")),
-            ListTile(
-                leading: const Icon(Icons.phone_iphone),
-                title: const Text("iOS")),
-            ListTile(leading: const Icon(Icons.web), title: const Text("Web")),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
+      body: (new ListView.builder(
+          itemCount: _counter,
+          itemBuilder: (context, index) {
+            int num = _counter - 1;
+            if (index == 0) {
+              return Text("There are " + "$num" + " Item");
+            }
+            return ListTile(
+                leading: const Icon(Icons.account_circle),
+                title: new Text("Item $index"));
+          })),
+
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
